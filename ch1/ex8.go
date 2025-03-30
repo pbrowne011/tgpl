9b7de7782f637171e82ce875fd04d5c342033276
Pat Browne <pjbrowne011@gmail.com>
2025-03-29 20:04:32 -0400

    more net/http exercises

---
+++ ch1/ex8.go
@@ -0,0 +1,44 @@
+// Exercise 1.8
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		// check for http prefix
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		// Use io.Copy() for unbufferd io
+		_, err = io.Copy(os.Stdout, resp.Body)
+
+		// Note: this line is essential. When working as a client, the package
+		// manages "Transport" type connections, reusing connections when
+		// efficient. This consumes file descriptors and network sockets in the
+		// OS. It is essential to close these resources after they are used to
+		// prevent OS errors from having too many conenctions open at once.
+		//
+		// Response.Body.Close() closes this network connection, freeing up
+		// network resources. I'm not sure how it does this (how does it keep
+		// track of network connections? why not resp.Close()?), but will dig
+		// into later.
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
